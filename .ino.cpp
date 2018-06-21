23b8174e3fd118a89675e6e0d584830bef3daa32
Frank Prüfer <frank.pruefer@itac.de>
2018-06-21 08:31:27 +0200

    renaming project and files

--- .ino.cpp
+++ .ino.cpp
@@ -2,24 +2,15 @@
 //This is a automatic generated file
 //Please do not modify this file
 //If you touch this file your change will be overwritten during the next build
-//This file has been generated on 2018-06-19 22:45:19
+//This file has been generated on 2018-06-21 08:30:28
 
 #include "Arduino.h"
-#include <Arduino.h>
-#include <HardwareSerial.h>
-#include <pins_arduino.h>
-#include <Print.h>
-void setup() ;
-void loop() ;
-void isr() ;
-double getDistance() ;
-double getDeltaDistance() ;
-double getSpeed(double hours, double kilometers) ;
-double microsToHours(unsigned long microSeconds) ;
-double microsToMinutes(unsigned long microSeconds) ;
-double microsToSeconds(unsigned long microSeconds) ;
 
-#include "MagneticSensor.ino"
+#error the file: EBikeController.ino is not found in the indexer though it exists on the file system.
+#error this is probably due to a bad eclipse configuration : ino and pde are not marked as c++ file.
+#error please check whether *.ino and *.pde are marked as C++ source code in windows->preferences->C/C++->file types.
+
+#include "EBikeController.ino"
 
 
 #endif
