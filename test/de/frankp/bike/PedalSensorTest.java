95e13e47e053977d06d88961cdf39fb072b2dc7a
Frank Prüfer <frank.pruefer@itac.de>
2018-06-28 15:06:56 +0200

    java source

---
+++ test/de/frankp/bike/PedalSensorTest.java
@@ -0,0 +1,14 @@
+package de.frankp.bike;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class PedalSensorTest {
+
+	@Test
+	void test() {
+		PedalSensor testee = new PedalSensor();
+	}
+
+}
